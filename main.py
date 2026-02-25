a3f5af3a3dae5684a373f8536e478cfe35a8eae8
ajhipsher <github.lcgx7@simplelogin.com>
2026-02-25 14:29:23 -0800

    Updated to allow user input.

--- main.py
+++ main.py
@@ -1,20 +1,33 @@
+import argparse
 import os
 
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
+parser = argparse.ArgumentParser(description="Chatbot")
+parser.add_argument("user_prompt", type=str, help="User prompt")
+args = parser.parse_args()
+
+messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
 
 def main():
     if api_key == None:
-        raise RuntimeError("Your API key isn't working!")
+        raise RuntimeError("Your API key isn't working.")
     response = client.models.generate_content(
         model="gemini-2.5-flash",
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+        contents=messages,
     )
+    if response.usage_metadata == None:
+        raise RuntimeError("Usage metadata was not returned.")
+    print(f"User Prompt: {args.user_prompt}")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print(response.text)
 
 
